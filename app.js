32a9db651a86572daa2f757aadfb09b2fbbd69e4
Jacquelyn <jtorborg1227@comcast.net>
2016-09-28 19:04:11 -0500

    base mode done

--- app.js
+++ app.js
@@ -4,27 +4,17 @@ $(document).ready(function() {
     // array of colors
     var colorArray = ['red', 'green', 'yellow', 'blue'];
 
-
-
     //append 4 block with jQuery
     createBlocks();
-
+    $('.textContainer').text("Please pick a block.");
 
     //createBlock function
     function createBlocks() {
         for (var i = 0; i < colorArray.length; i++) {
             $('.container').append("<div class='" + colorArray[i] + "'></div>");
-            var colorData = $('.container').closest().data(working: "Yes");
-            console.log(colorData);
-
+            $('.container').children().last().data('id', colorArray[i]);
         }
     }
-
-
-    //associate number with block
-
-
-
 
     //Prompt the player to pick one of the color blocks you created at random.
     $('.textContainer').text("Please pick a block.");
@@ -34,9 +24,6 @@ $(document).ready(function() {
         return Math.floor(Math.random() * (1 + max - min) + min);
     }
 
-
-
-
     //event listeners
     $('.red').on('click', randomColor);
     $('.green').on('click', randomColor);
@@ -45,24 +32,37 @@ $(document).ready(function() {
 
     function randomColor() {
         var result = randomNumber(0, 4);
-        console.log(result);
-        console.log()
-        console.log(this);
+        console.log("Result of random number function:", result);
+        console.log("This:", this);
+
         var className = $(this).attr('class');
-        console.log(className);
-        //if className == colorArray index (id) data value
+        var colorData = $(this).data('id');
+        console.log("Data on div:", colorData);
+
+        var chosen = colorArray.indexOf(colorData);
+        console.log("Array index number of color chosen:", chosen);
+        //var chosen2 = colorArray.indexOf(className);
+        //console.log(chosen2);
+
+        //If the player clicks on the correct color block, give them a success message.
+        //Prompt the player to pick another block randomly and let them keep playing.
+
+        if (chosen == result) {
+            $('.textContainer').text("Good job. Please keep playing");
+        } else {
+          //If they pick the wrong color, let them know.
+
+            $('.textContainer').text("Please try again.");
+        }
     }
 
 
-    //If the player clicks on the correct color block, give them a success message.
-    //Prompt the player to pick another block randomly and let them keep playing.
+
 
     //if (color picked == random color) {
-    //$('.textContainer').text("Please pick a block.");
 
     //alert("Good job. Please reload the page and keep playing")
     //} else {
-    //If they pick the wrong color, let them know.
     //
     //alert("Please try again.")
     //}
