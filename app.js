cc8574c800627cfd0597fb9e968e39f12c221784
Jacquelyn <jtorborg1227@comcast.net>
2016-10-02 11:04:18 -0500

    revamped for UX

--- app.js
+++ app.js
@@ -3,26 +3,29 @@ console.log("app.js is working");
 $(document).ready(function() {
     // array of colors
     var colorArray = ['red', 'green', 'yellow', 'blue'];
-
+    var result;
     //append 4 block with jQuery
     createBlocks();
-    $('.textContainer').text("Please pick a block.");
+    chooseRandomBlock();
 
-    //createBlock function
-    function createBlocks() {
-        for (var i = 0; i < colorArray.length; i++) {
-            $('.container').append("<div class='" + colorArray[i] + "'></div>");
-            $('.container').children().last().data('id', colorArray[i]);
-        }
-    }
+
+    //$('.textContainer').text("Please click on the " + colorArray[result] + " block.");
+
+//     //Run the initial random function
+//     chooseColor()
+//
+//     //Initial random function
+//     function chooseColor () {
+//
+// var result = randomNumber(0, 3);
+// var firstClass = colorArray[result];
+// console.log(firstClass);
+// $('.textContainer').text("Please click the " + firstClass + " block.");
+//
+//     }
+
 
     //Prompt the player to pick one of the color blocks you created at random.
-    $('.textContainer').text("Please pick a block.");
-
-
-    function randomNumber(min, max) {
-        return Math.floor(Math.random() * (1 + max - min) + min);
-    }
 
     //event listeners
     $('.red').on('click', randomColor);
@@ -31,8 +34,11 @@ $(document).ready(function() {
     $('.blue').on('click', randomColor);
 
     function randomColor() {
-        var result = randomNumber(0, 3);
-        console.log("Result of random number function:", result);
+
+      //chooseRandomBlock();
+      //var result = randomNumber(0, 3);
+
+        //console.log("Result of random number function:", result);
         console.log("This:", this);
 
         var className = $(this).attr('class');
@@ -41,21 +47,22 @@ $(document).ready(function() {
 
         var chosen = colorArray.indexOf(colorData);
         console.log("Array index number of color chosen:", chosen);
-        //var chosen2 = colorArray.indexOf(className);
-        //console.log(chosen2);
+        var chosen2 = colorArray.indexOf(className);
+        console.log("Result", result);
+        console.log(chosen2);
+        $('.textContainer').text("Please click on the " + colorArray[colorData] + " block.");
 
         //If the player clicks on the correct color block, give them a success message.
         //Prompt the player to pick another block randomly and let them keep playing.
-
         if (chosen == result) {
-            $('.textContainer').text("Good job. Please keep playing");
-        } else {
-          //If they pick the wrong color, let them know.
+            $('.textContainer').text("Good job. Please play again.");
 
+        } else {
+            //If they pick the wrong color, let them know.
             $('.textContainer').text("Please try again.");
         }
-    }
 
+    }
 
 
 
@@ -67,8 +74,29 @@ $(document).ready(function() {
     //alert("Please try again.")
     //}
 
+    function randomNumber(min, max) {
+        return Math.floor(Math.random() * (1 + max - min) + min);
+    }
 
 
+
+    //createBlock function
+    function createBlocks() {
+        for (var i = 0; i < colorArray.length; i++) {
+            $('.container').append("<div class='" + colorArray[i] + "'></div>");
+            $('.container').children().last().data('id', colorArray[i]);
+
+        }
+    }
+
+    function chooseRandomBlock() {
+      result = randomNumber(0, 3);
+      console.log("Result of random number function:", result);
+      $('.textContainer').text("Please click on the " + colorArray[result] + " block.");
+      return result;
+
+
+    }
 
 
 });
